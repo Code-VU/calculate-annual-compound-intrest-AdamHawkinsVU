50119f735b7f7a2a06ce7233d5ac771b4c662253
Adam Hawkins <Adam.Hawkins@vu.com>
2022-02-15 07:48:56 -0600

    first draft

--- compoundInterest.py
+++ compoundInterest.py
@@ -2,9 +2,19 @@ def calculateCompoundInterest():
     
 # This first 3 lines are provided for yougetACompoundIntrest()
 # This first 3 lines are provided for you
- client_one_principal = float(input("Principle (amount): "))
- client_one_time =      float(input("Time:               "))
- client_one_rate =      float(input("Rate:               "))
+ x=0
+ while x<3 :
+    client_one_principal = float(input("Principle (amount): "))
+    client_one_time = float(input("Time:               "))
+    client_one_rate = float(input("Rate:               "))
+    amount = client_one_principal*(pow((1+client_one_rate/100), client_one_time))
+    comp_int = amount - client_one_principal
+    print("Compound Interest: ",comp_int)
+    x=x+1
+    if x<=2:
+        print("---")
+    
+
  #print("Compound Interest: "+str(intrest))
 
     # end assignment
